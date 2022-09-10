00762a24c86ca7d9a4caced2f6ac5c70226f0b87
RicardoRamautar <80088597+RicardoRamautar@users.noreply.github.com>
2022-09-10 08:57:33 +0200

    working on graphColoring_Backtracking

--- ColoringProblem.py
+++ ColoringProblem.py
@@ -1,9 +1,12 @@
 # functions
+def graphColoring_BackTracking(m, graph):
+    VertexColors = [0,0,0,0]        # initialize array storing colors of Vertices
+
+    if graphColoring(m, VertexColors, graph) == False:
+        '''returns false if there is no possible solution'''
+        return False
 
 
 # input variables
 graph = [[0, 1, 1, 1], [1, 0, 1, 0], [1, 1, 0, 1], [1, 0, 1, 0]]
-m     = 
-
-# global variables
-V     = len(graph)
+m     = 3           # number of colors
