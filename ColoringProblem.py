5a803ceecffda2058e4895574a7c1521ba9a720a
RicardoRamautar <80088597+RicardoRamautar@users.noreply.github.com>
2022-09-10 09:11:40 +0200

    inter

--- ColoringProblem.py
+++ ColoringProblem.py
@@ -1,10 +1,32 @@
 # functions
-def graphColoring_BackTracking(m, graph):
-    VertexColors = [0,0,0,0]        # initialize array storing colors of Vertices
 
-    if graphColoring(m, VertexColors, graph) == False:
-        '''returns false if there is no possible solution'''
-        return False
+def colorGraph(m, vertexColors, graph, vIdx):
+    nrVertices = len(graph)
+
+    if vIdx == nrVertices:
+        return True
+
+    for i in range(1, m+1):
+        if isPossible(vIdx, vertexColors, i, graph) == True:
+            vertexColors[vIdx] = i
+
+            if colorGraph(m, vertexColors, graph, vIdx+1) == True:
+                return True
+
+            vertexColors[vIdx] = 0
+
+def graphColoring_BackTracking(m, graph):
+    vertexColors = [0 for i in range(len(graph))]        # initialize array storing colors of Vertices
+
+    solution = False
+    m = 1
+    while solution == False:
+        if colorGraph(m, vertexColors, graph, 0) == True:
+            solution = True
+        else:
+            m += 1
+            
+    
 
 
 # input variables
