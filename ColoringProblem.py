b460f66b20737a4db67407e983099b3c9b270abb
RicardoRamautar <80088597+RicardoRamautar@users.noreply.github.com>
2022-09-10 09:19:34 +0200

    finished

--- ColoringProblem.py
+++ ColoringProblem.py
@@ -1,5 +1,14 @@
 # functions
 
+def isPossible(vIdx, vertexColors, i, graph, nrVertices):
+    for j in range(nrVertices):
+        if graph[vIdx][j] == 1 and vertexColors[j] == i:
+            # graph[vIdx][j] == 1 checks if vertices vIdx and j are linked
+            # vertexColors[j] == i checks if vertex j is of color i
+            return False
+
+    return True
+
 def colorGraph(m, vertexColors, graph, vIdx):
     nrVertices = len(graph)
 
@@ -7,7 +16,7 @@ def colorGraph(m, vertexColors, graph, vIdx):
         return True
 
     for i in range(1, m+1):
-        if isPossible(vIdx, vertexColors, i, graph) == True:
+        if isPossible(vIdx, vertexColors, i, graph, nrVertices) == True:
             vertexColors[vIdx] = i
 
             if colorGraph(m, vertexColors, graph, vIdx+1) == True:
@@ -15,7 +24,7 @@ def colorGraph(m, vertexColors, graph, vIdx):
 
             vertexColors[vIdx] = 0
 
-def graphColoring_BackTracking(m, graph):
+def graphColoring_BackTracking(graph):
     vertexColors = [0 for i in range(len(graph))]        # initialize array storing colors of Vertices
 
     solution = False
@@ -23,6 +32,7 @@ def graphColoring_BackTracking(m, graph):
     while solution == False:
         if colorGraph(m, vertexColors, graph, 0) == True:
             solution = True
+            print(vertexColors)
         else:
             m += 1
             
@@ -30,5 +40,6 @@ def graphColoring_BackTracking(m, graph):
 
 
 # input variables
-graph = [[0, 1, 1, 1], [1, 0, 1, 0], [1, 1, 0, 1], [1, 0, 1, 0]]
-m     = 3           # number of colors
+graph = [[0, 1, 1, 1, 1], [1, 0, 1, 0, 1], [1, 1, 0, 1, 0], [1, 0, 1, 0, 1], [1, 0, 1, 1, 0]]
+
+graphColoring_BackTracking(graph)
