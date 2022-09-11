7c6f25f8d79bda07b2feb3fbd44005f7db6694b6
RicardoRamautar <80088597+RicardoRamautar@users.noreply.github.com>
2022-09-11 19:09:14 +0200

    Finished M Coloring Problem

--- ColoringProblem.py
+++ ColoringProblem.py
@@ -3,8 +3,6 @@
 def isPossible(vIdx, vertexColors, i, graph, nrVertices):
     for j in range(nrVertices):
         if graph[vIdx][j] == 1 and vertexColors[j] == i:
-            # graph[vIdx][j] == 1 checks if vertices vIdx and j are linked
-            # vertexColors[j] == i checks if vertex j is of color i
             return False
 
     return True
@@ -23,10 +21,9 @@ def colorGraph(m, vertexColors, graph, vIdx):
                 return True
 
             vertexColors[vIdx] = 0
-    colorGraph(m, vertexColors, graph, vIdx+1)
 
 def graphColoring_BackTracking(graph):
-    vertexColors = [0 for i in range(len(graph))]        # initialize array storing colors of Vertices
+    vertexColors = [0 for i in range(len(graph))]
 
     m = 2
     while True:
@@ -41,7 +38,7 @@ def graphColoring_BackTracking(graph):
 
 # input variables
 # graph = [[0, 1, 1, 1, 1], [1, 0, 1, 0, 1], [1, 1, 0, 1, 0], [1, 0, 1, 0, 1], [1, 0, 1, 1, 0]]
-graph = [[0, 1, 1, 1], [1, 0, 1, 0], [1, 1, 0, 1], [1, 0, 1, 0]]
-# graph = [[0, 1, 1, 1, 1, 0], [1, 0, 1, 0, 1, 1], [1, 1, 0, 1, 0, 1], [1, 0, 1, 0, 1, 0], [1, 0, 1, 1, 0, 1], [0, 1, 1, 1, 1, 0]]
+# graph = [[0, 1, 1, 1], [1, 0, 1, 0], [1, 1, 0, 1], [1, 0, 1, 0]]
+graph = [[0, 1, 1, 1, 1, 0], [1, 0, 1, 0, 1, 1], [1, 1, 0, 1, 0, 1], [1, 0, 1, 0, 1, 0], [1, 1, 1, 1, 0, 1], [0, 1, 1, 0, 1, 0]]
 
 print(graphColoring_BackTracking(graph))
