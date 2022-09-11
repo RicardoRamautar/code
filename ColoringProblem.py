70385710ed51222592e7c0de398a54c565ae0f30
RicardoRamautar <80088597+RicardoRamautar@users.noreply.github.com>
2022-09-11 19:47:40 +0200

    Improved isPossible check and added test cases

--- ColoringProblem.py
+++ ColoringProblem.py
@@ -1,8 +1,8 @@
 def isPossible(vIdx, vertexColors, i, graph, nrVertices):
-    for j in range(nrVertices):
-        if graph[vIdx][j] == 1 and vertexColors[j] == i:
+    neighbours = graph[vIdx]
+    for j in neighbours:
+        if vertexColors[j-1] == i:
             return False
-
     return True
 
 def colorGraph(m, vertexColors, graph, vIdx):
@@ -26,15 +26,43 @@ def graphColoring_BackTracking(graph):
     m = 2
     while True:
         if colorGraph(m, vertexColors, graph, 0) == True:
-            print(vertexColors)
-            return m
+            return vertexColors
         else:
             m += 1
-            
-    
 
-# graph = [[0, 1, 1, 1, 1], [1, 0, 1, 0, 1], [1, 1, 0, 1, 0], [1, 0, 1, 0, 1], [1, 0, 1, 1, 0]]
-# graph = [[0, 1, 1, 1], [1, 0, 1, 0], [1, 1, 0, 1], [1, 0, 1, 0]]
-graph = [[0, 1, 1, 1, 1, 0], [1, 0, 1, 0, 1, 1], [1, 1, 0, 1, 0, 1], [1, 0, 1, 0, 1, 0], [1, 1, 1, 1, 0, 1], [0, 1, 1, 0, 1, 0]]
+res1 = graphColoring_BackTracking([(2,3,4),(1,3,4),(1,2),(1,2)])
+res2 = graphColoring_BackTracking([(2,4),(1,3),(2,4),(1,3)])
+res3 = graphColoring_BackTracking([(4,5),(3,4),(2,4),(1,2,3),(1,4)])
+res4 = graphColoring_BackTracking([(2,3,4),(1,3,5),(1,2,5),(1,5),(2,3,4)])
+res5 = graphColoring_BackTracking([(2,3,4,6),(1,3,5),(1,2,5),(1,6),(2,3,6),(1,4,5)])
+res6 = graphColoring_BackTracking([(2,3,4,6),(1,3,4,5),(1,2,5),(1,2,6),(2,3,6),(1,4,5)])
 
-print(graphColoring_BackTracking(graph))
+if res1 == [1,2,3,3]:
+    print("Test 1: Correct")
+else:
+    print("Test 1: FALSE!!!")
+
+if res2 == [1,2,1,2]:
+    print("Test 2: Correct")
+else:
+    print("Test 2: FALSE!!!")
+
+if res3 == [1,1,2,3,2]:
+    print("Test 3: Correct")
+else:
+    print("Test 3: FALSE!!!")
+
+if res4 == [1,2,3,2,1]:
+    print("Test 4: Correct")
+else:
+    print("Test 4: FALSE!!!")
+
+if res5 == [1,2,3,2,1,3]:
+    print("Test 5: Correct")
+else:
+    print("Test 5: FALSE!!!")
+
+if res6 == [1,2,3,3,1,2]:
+    print("Test 6: Correct")
+else:
+    print("Test 6: FALSE!!!")
